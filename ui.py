2b38137821d0ff5af5b5da61db372bd5fd6eaed4
Zhaoyi Yan <priscillayouziqian@gmail.com>
2023-05-18 21:29:12 -0400

    initial commit

---
+++ ui.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+THEME_COLOR = "#375362"
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label()(text="Score: 0", fg="white")
+
+        self.window.mainloop()
